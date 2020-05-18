09ca4f3cae4e02f72e6d8bf1162920e53c196edb
adkantor <adkantor@users.noreply.github.com>
2020-05-18 14:33:58 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -5,6 +5,8 @@
 BYTE get_RGB_average(BYTE r, BYTE g, BYTE b);
 void copy_image(int height, int width, RGBTRIPLE original[height][width], RGBTRIPLE copy[height][width]);
 RGBTRIPLE get_box_blur(int n, RGBTRIPLE box[n]);
+RGBTRIPLE black_pixel(void);
+RGBTRIPLE edgeSobel(int n, RGBTRIPLE box[n]);
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -100,6 +102,48 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    // copy image
+    RGBTRIPLE img_copy[height][width];
+    copy_image(height, width, image, img_copy);
+
+    // iterate individual pixels
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // get Box
+            int box_height = 3;
+            int box_width = 3;
+            RGBTRIPLE box[box_height * box_width]; // flattened
+            int box_ndx = 0;
+            for (int img_r = i - 1; img_r <= i + 1; img_r++)
+            {
+                // if (img_r >= 0 && img_r <= height - 1)
+                {
+                    for (int img_c = j - 1; img_c <= j + 1; img_c++)
+                    {
+                        if (img_r >= 0 && img_r <= height - 1 && img_c >= 0 && img_c <= width - 1)
+                        {
+                            box[box_ndx] = img_copy[img_r][img_c];
+                        }
+                        else
+                        {
+                            // black pixel
+                            box[box_ndx] = black_pixel();
+                        }
+                        box_ndx++;
+                    }
+                }
+            }
+
+            // calculate pixel color
+            RGBTRIPLE pixel_new = edgeSobel(box_height * box_width, box);
+
+            // change pixel to new
+            image[i][j] = pixel_new;
+        }
+    }
+
     return;
 }
 
@@ -151,6 +195,53 @@ RGBTRIPLE get_box_blur(int n, RGBTRIPLE box[n])
     temp.rgbtRed = round((double) sumRed / n);
     temp.rgbtGreen = round((double) sumGreen / n);
     temp.rgbtBlue = round((double) sumBlue / n);
+
+    return temp;
+}
+
+// Returns a black pixel (RGB=0)
+RGBTRIPLE black_pixel(void)
+{
+    RGBTRIPLE temp;
+    temp.rgbtRed = 0;
+    temp.rgbtGreen = 0;
+    temp.rgbtBlue = 0;
+
+    return temp;
+}
+
+// Returns the final RGB color of a pixel after applying edge filter, using Sobel algorithm.
+// @param n         array length (must be 9)
+// @param box       flattened array of box
+// Return           RGB color of pixel after applying edge filter
+RGBTRIPLE edgeSobel(int n, RGBTRIPLE box[n])
+{
+    int Gx_Red = 0, Gx_Green = 0, Gx_Blue = 0;
+    int Gy_Red = 0, Gy_Green = 0, Gy_Blue = 0;
+    int Gx_mask[9] = {-1, 0, 1, -2, 0, 2, -1, 0, 1};
+    int Gy_mask[9] = {-1, -2, -1, 0, 0, 0, 1, 2, 1};
+    for (int i = 0; i < n; i++)
+    {
+        // Gx channels
+        Gx_Red += box[i].rgbtRed * Gx_mask[i];
+        Gx_Green += box[i].rgbtGreen * Gx_mask[i];
+        Gx_Blue += box[i].rgbtBlue * Gx_mask[i];
+        // Gy channels
+        Gy_Red += box[i].rgbtRed * Gy_mask[i];
+        Gy_Green += box[i].rgbtGreen * Gy_mask[i];
+        Gy_Blue += box[i].rgbtBlue * Gy_mask[i];
+    }
+
+    // final colors
+    BYTE final_Red = round(fmin(sqrt(pow((double)Gx_Red, 2) + pow((double)Gy_Red, 2)), 255.));
+    BYTE final_Green = round(fmin(sqrt(pow((double)Gx_Green, 2) + pow((double)Gy_Green, 2)), 255.));
+    BYTE final_Blue = round(fmin(sqrt(pow((double)Gx_Blue, 2) + pow((double)Gy_Blue, 2)), 255.));
+
+
+    RGBTRIPLE temp;
+    temp.rgbtRed = final_Red;
+    temp.rgbtGreen = final_Green;
+    temp.rgbtBlue = final_Blue;
 
     return temp;
 }
